214d871d39ea3208dc240ee41aa23fdcbb204e3a
taufiqm <fiqmurtadho@gmail.com>
2021-06-17 11:29:40 +0900

    define method1_hamiltonian function

--- src/QuantumXY.jl
+++ src/QuantumXY.jl
@@ -1,1 +1,25 @@
+using LinearAlgebra
 
+function method1_hamiltonian(h::Float64, J::Float64, N::Int64)
+    #Generating all possible bit sequence of length N
+    mat = zeros(2^N, 2^N)
+    bitsequence = reverse([reverse(digits(UInt(i), base =2, pad = N)) for i in 0:2^N-1])
+    for sequence in bitsequence
+        if sequence[N] != sequence[1]
+            newsequence = copy(sequence)
+            newsequence[N], newsequence[1] = sequence[1], sequence[N]
+            mat[findfirst(isequal(sequence), bitsequence), findfirst(isequal(newsequence), bitsequence)]+= -J
+        end
+        for i in 1:N-1
+            if sequence[i]!=sequence[i+1]
+                newsequence = copy(sequence)
+                newsequence[i], newsequence[i+1] = sequence[i+1], sequence[i]
+                mat[findfirst(isequal(sequence), bitsequence), findfirst(isequal(newsequence), bitsequence)]+= -J
+            end
+        end
+    end
+    for i in 1:2^N
+        mat[i,i] = -h*sum(bitsequence[i]) + h*(N-sum(bitsequence[i]))
+    end
+    return mat
+end
